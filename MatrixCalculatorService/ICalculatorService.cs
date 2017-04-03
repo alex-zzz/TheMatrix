bb5faca588aee6608b93f325d5382dd309bc44a7
Дмитрий Гончар <dymych@outlook.com>
2017-04-03 18:42:16 +0300

    Класс Калькулятор

--- MatrixCalculatorService/ICalculatorService.cs
+++ MatrixCalculatorService/ICalculatorService.cs
@@ -8,6 +8,17 @@ namespace MatrixCalculatorService
 {
     interface ICalculatorService
     {
+        double[,] Add(double[,] A, double[,] B);
+        double[,] Subtract(double[,] A, double[,] B);
+        double[,] Mult(double[,] A, double[,] B);
+        double[,] Divide(double[,] A, double[,] B);
 
+        double[,] Transposing(double[,] A);
+        double[,] Reverse(double[,] A);
+        double[,] MultOn(double[,] A, double number);
+        double[,] DivideOn(double[,] A, double number);
+
+        double[,] GetZeroMatrix(int size);
+        double[,] GetIdentityMatrix(int size);
     }
 }

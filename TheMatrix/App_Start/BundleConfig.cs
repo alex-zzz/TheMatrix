32124d94ad1626ae422508e1f422aabc0e7083fd
Alex <zzzqqq@outlook.com>
2017-04-04 12:00:02 +0300

    Matrixs

--- TheMatrix/App_Start/BundleConfig.cs
+++ TheMatrix/App_Start/BundleConfig.cs
@@ -14,6 +14,11 @@ namespace TheMatrix
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jqgrid").Include(
+                        "~/Scripts/jquery-ui-1.10.0.min.js",
+                        "~/Scripts/jquery.jqGrid.min.js",
+                        "~/Scripts/i18n/grid.locale-en.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -26,6 +31,10 @@ namespace TheMatrix
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/jqgrid").Include(
+                      "~/Content/themes/base/jquery.ui.all.css",
+                      "~/Content/jquery.jqGrid/ui.jqgrid.css"));
         }
     }
 }
